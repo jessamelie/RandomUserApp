1d71b857422945241f766ac1e1e7fe329a204dc0
jessamelie <jessamelie.sola@gmail.com>
2023-09-06 01:51:28 +0200

    stylizing users cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Card, CardMedia } from "@mui/material";
+import '../style/Cards.css'
 
-const Cards = ({user}) => {
-    return (
-        <div className='userDetails'>
-            <img src={user.picture.large} alt="user profile picture"/>
-            <h1>{user.name.first}</h1>
-            <h1>{user.name.last}</h1>
-            <h2>{user.location.country}</h2>
-        </div>
-    );
+const Cards = ({ user }) => {
+  return (
+    <div className="cardsContainer">
+      <div className="userCard">
+        <Card sx={{ maxWidth: 400 }}>
+          <CardMedia
+            component="img"
+            height="250"
+            image={user.picture.large}
+            alt="user profile picture"
+          />
+          <h1>{user.name.first}</h1>
+          <h1>{user.name.last}</h1>
+          <h2>{user.location.country}</h2>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
