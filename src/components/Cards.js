6f772bec701a3133da0ab27ced0819518f997270
jessamelie <jessamelie.sola@gmail.com>
2023-09-06 01:16:40 +0200

    styling files adding

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Cards = ({user}) => {
     return (
         <div className='userDetails'>
-            <img src={user.picture.medium} alt="user profile picture"/>
+            <img src={user.picture.large} alt="user profile picture"/>
             <h1>{user.name.first}</h1>
             <h1>{user.name.last}</h1>
-            <h1>{user.location.country}</h1>
+            <h2>{user.location.country}</h2>
         </div>
     );
 };
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Users from '../components/Users';
+import '../style/Homepage.css';
 
 const Homepage = () => {
     return (
